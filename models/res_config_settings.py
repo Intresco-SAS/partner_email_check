68591fb79ebe5ffb426a9f31653c5cff7e5860da
Nelson V <nelson.avelez@hotmail.com>
2021-11-18 17:57:51 -0500

    14.0

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -2,41 +2,26 @@ from odoo import api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
+
+    partner_email_check_syntax = fields.Boolean(
+        related="company_id.partner_email_check_syntax",
+        readonly=False,
+    )
 
     partner_email_check_filter_duplicates = fields.Boolean(
-        string="Filtrar correos electrónicos duplicados de contactos",
-        help="No permita que varios contactos tengan el mismo correo electrónico.",
+        related="company_id.partner_email_check_filter_duplicates",
+        readonly=False,
     )
 
     partner_email_check_check_deliverability = fields.Boolean(
-        string="Verificar la capacidad de entrega de las direcciones de correo electrónico",
-        help="No permite direcciones de correo electrónico con proveedores que no existen",
+        related="company_id.partner_email_check_check_deliverability",
+        readonly=False,
     )
 
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        conf = self.env['ir.config_parameter'].sudo()
-        res.update(
-            partner_email_check_filter_duplicates=conf.get_param(
-                'partner_email_check_filter_duplicates', 'False'
-            ) == 'True',
-            partner_email_check_check_deliverability=conf.get_param(
-                'partner_email_check_check_deliverability', 'False'
-            ) == 'True',
-        )
-        return res
-
-    @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        conf = self.env['ir.config_parameter'].sudo()
-        conf.set_param(
-            'partner_email_check_filter_duplicates',
-            self.partner_email_check_filter_duplicates
-        )
-        conf.set_param(
-            'partner_email_check_check_deliverability',
-            self.partner_email_check_check_deliverability
-        )
+    @api.onchange(
+        "partner_email_check_syntax", "partner_email_check_check_deliverability"
+    )
+    def _onchange_partner_email_check_check_deliverability(self):
+        if self.partner_email_check_check_deliverability:
+            self.partner_email_check_syntax = True
--- models/res_partner.py
+++ models/res_partner.py
@@ -2,17 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 import logging
-from odoo import api, models, _
+
+from odoo import _, api, models
 from odoo.exceptions import UserError, ValidationError
-from odoo.tools import config
 
 _logger = logging.getLogger(__name__)
 
 try:
     from email_validator import (
-        validate_email,
         EmailSyntaxError,
         EmailUndeliverableError,
+        validate_email,
     )
 except ImportError:
     _logger.debug('Cannot import "email_validator".')
@@ -21,38 +21,48 @@ except ImportError:
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
+
+    def copy_data(self, default=None):
+        res = super(ResPartner, self).copy_data(default=default)
+        if self._should_filter_duplicates():
+            for copy_vals in res:
+                copy_vals.pop("email", None)
+        return res
 
     @api.model
     def email_check(self, emails):
-        if (config['test_enable'] and
-                not self.env.context.get('test_partner_email_check')):
-            return emails
-        return ','.join(self._normalize_email(email.strip())
-                        for email in emails.split(','))
+        return ",".join(
+            self._normalize_email(email.strip())
+            for email in emails.split(",")
+            if email.strip()
+        )
 
-    @api.constrains('email')
+    @api.constrains("email")
     def _check_email_unique(self):
         if self._should_filter_duplicates():
             for rec in self.filtered("email"):
-                if ',' in rec.email:
+                if "," in rec.email:
                     raise UserError(
-                        _("Field contains multiple email addresses. This is "
-                          "not supported when duplicate email addresses are "
-                          "not allowed.")
+                        _(
+                            "Field contains multiple email addresses. This is "
+                            "not supported when duplicate email addresses are "
+                            "not allowed."
+                        )
                     )
-                if self.search_count(
-                    [('email', '=', rec.email), ('id', '!=', rec.id)]
-                ):
+                if self.search_count([("email", "=", rec.email), ("id", "!=", rec.id)]):
                     raise UserError(
                         _("Email '%s' is already in use.") % rec.email.strip()
                     )
 
     def _normalize_email(self, email):
+        if not self._should_check_syntax():
+            return email
         if validate_email is None:
             _logger.warning(
-                'Can not validate email, '
-                'python dependency required "email_validator"')
+                "Can not validate email, "
+                'python dependency required "email_validator"'
+            )
             return email
 
         try:
@@ -61,35 +71,29 @@ class ResPartner(models.Model):
                 check_deliverability=self._should_check_deliverability(),
             )
         except EmailSyntaxError:
-            raise ValidationError(
-                _("%s Es un correo electrónico invalido, verifique de nuevo el formato") % email.strip()
-            )
+            raise ValidationError(_("%s is an invalid email") % email.strip())
         except EmailUndeliverableError:
             raise ValidationError(
-                _("Verifique de nuevo el correo, No es posible enviar mensajes a este correo electrónico: %s") % email.strip()
+                _("Cannot deliver to email address %s") % email.strip()
             )
-        return result['local'].lower() + '@' + result['domain_i18n']
+        return result["local"].lower() + "@" + result["domain_i18n"]
+
+    def _should_check_syntax(self):
+        return self.env.company.partner_email_check_syntax
 
     def _should_filter_duplicates(self):
-        conf = self.env['ir.config_parameter'].sudo().get_param(
-            'partner_email_check_filter_duplicates', 'False'
-        )
-        return conf == 'True'
+        return self.env.company.partner_email_check_filter_duplicates
 
     def _should_check_deliverability(self):
-        conf = self.env['ir.config_parameter'].sudo().get_param(
-            'partner_email_check_check_deliverability', 'False'
-        )
-        return conf == 'True'
+        return self.env.company.partner_email_check_check_deliverability
 
     @api.model
     def create(self, vals):
-        if vals.get('email'):
-            vals['email'] = self.email_check(vals['email'])
+        if vals.get("email"):
+            vals["email"] = self.email_check(vals["email"])
         return super(ResPartner, self).create(vals)
 
-    @api.multi
     def write(self, vals):
-        if vals.get('email'):
-            vals['email'] = self.email_check(vals['email'])
+        if vals.get("email"):
+            vals["email"] = self.email_check(vals["email"])
         return super(ResPartner, self).write(vals)
